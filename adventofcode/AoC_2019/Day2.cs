32bcabd260683ab6635d985e693a64e3d1b629a7
Jonas Högström <jonas.hogstrom@tobii.com>
2019-12-02 08:21:44 +0100

    refactored the intcomputer

--- adventofcode/AoC_2019/Day2.cs
+++ adventofcode/AoC_2019/Day2.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Net.Mail;
-using System.Windows.Forms;
 
 namespace adventofcode.AoC_2019
 {
@@ -17,62 +14,27 @@ namespace adventofcode.AoC_2019
             Part1TestSolution = 30;
             Part2TestSolution = null;
             Part1Solution = 2692315;
-            Part2Solution = null;
+            Part2Solution = 9507;
         }
 
         protected override void DoRun(string[] input)
         {
-
-            Part1 = runprogram(input[0], 12, 02);
-            
-
-            for (var noun=0; noun<100; noun++)
-                for (var verb= 0; verb< 100; verb++)
-                    if (runprogram(input[0], noun, verb) == 19690720)
-                    {
-                        Part2 = 100 * noun + verb;
-                        return;
-                    }
+            var comp = new IntCodeComputer();
+            Part1 = comp.runprogram(input[0], 12, 02);
+         
+            for (var noun = 0; noun < 100; noun++)
+            for (var verb = 0; verb < 100; verb++)
+            {
+                var res = comp.runprogram(input[0], noun, verb);
+                if (res == 19690720)
+                {
+                    Part2 = 100 * noun + verb;
+                    return;
+                }
+            }
 
         }
 
-        private int runprogram(string program, int noun, int verb)
-        {
-            var mem = GetInts(program).ToArray();
-            var pointer = 0;
 
-            if (Source == InputSource.prod)
-            {
-                mem[1] = noun;
-                mem[2] = verb;
-            }
-
-            while (pointer < mem.Length)
-            {
-                var op = mem[pointer];
-                if (op == 99)
-                {
-                    return mem[0];
-                }
-
-                var arg1 = mem[pointer + 1];
-                var arg2 = mem[pointer + 2];
-                var res = mem[pointer + 3];
-
-                switch (op)
-                {
-                    case 1:
-                        mem[res] = mem[arg1] + mem[arg2];
-                        break;
-                    case 2:
-                        mem[res] = mem[arg1] * mem[arg2];
-                        break;
-                }
-
-                pointer += 4;
-            }
-
-            throw new Exception("program didn't terminate...");
-        }
     }
 }
---
+++ adventofcode/AoC_2019/IntCodeComputer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode.AoC_2019
+{
+    public class IntCodeComputer
+    {
+        public int[] memory;
+        public int pointer;
+        public bool terminated { get; set; }
+
+        private readonly Dictionary<int, IOperation> _ops = new Dictionary<int, IOperation>();
+
+        public IntCodeComputer()
+        {
+            RegisterOp(new Add());
+            RegisterOp(new Mul());
+            RegisterOp(new Quit());
+        }
+
+        private void RegisterOp(IOperation op)
+        {
+            _ops[op.OpCode] = op;
+        }
+
+        public int runprogram(string program, int noun, int verb)
+        {
+            // Console.WriteLine($"======================= {noun} {verb}");
+            memory = program.Split(',').Select(int.Parse).ToArray();
+            pointer = 0;
+            memory[1] = noun;
+            memory[2] = verb;
+            terminated = false;
+
+            while (!terminated)
+            {
+                if (pointer >= memory.Length)
+                    throw new Exception("pointer outside of memory");
+                if (!_ops.ContainsKey(memory[pointer]))
+                    throw new Exception("unknown operation");
+                var op = _ops[memory[pointer]];
+                op.Execute(this);
+            }
+
+            return memory[0];
+        }
+    }
+
+    internal class Add : IntFunc
+    {
+        public override int OpCode => 1;
+
+        protected override int calc(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+    }
+
+    internal abstract class IntFunc : BaseOp, IOperation
+    {
+        public abstract int OpCode { get; }
+        public int ArgCount => 3;
+        public void Execute(IntCodeComputer comp)
+        {
+            var args = GetArgs(comp, ArgCount);
+
+            int a1 = comp.memory[args[0]];
+            var a2 = comp.memory[args[1]];
+            var res = calc(a1, a2);
+            // Console.WriteLine($"{a1} ({opCode}) {a2} => {res}");
+            comp.memory[args[2]] = res;
+            comp.pointer += ArgCount+1;
+        }
+
+        protected abstract int calc(int arg1, int arg2);
+    }
+
+    internal class Mul : IntFunc
+    {
+        public override int OpCode => 2;
+        protected override int calc(int arg1, int arg2)
+        {
+            return arg1 * arg2;
+        }
+    }
+
+    public class BaseOp
+    {
+        protected int[] GetArgs(IntCodeComputer comp, int argCount)
+        {
+            var args = new int[argCount];
+            for (int i = 0; i < argCount; i++)
+            {
+                args[i] = comp.memory[comp.pointer + i + 1];
+            }
+
+            return args;
+        }
+    }
+
+    internal class Quit : BaseOp, IOperation
+    {
+        public int OpCode => 99;
+        public int ArgCount => 0;
+        public void Execute(IntCodeComputer comp)
+        {
+            comp.terminated = true;
+        }
+    }
+
+    interface IOperation
+    {
+        int OpCode { get; }
+        int ArgCount { get; }
+        void Execute(IntCodeComputer comp);
+    }
+}
