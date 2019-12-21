8f146c431317c723c28883b009c11497e21287f6
Jonas Högström <jonas.hogstrom@tobii.com>
2019-12-21 05:57:13 +0100

    day21 empty

---
+++ adventofcode/AoC_2019/Day21.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventofCode.AoC_2019
+{
+    using Part1Type = Int32;
+    using Part2Type = Int32;
+
+    [TestFixture]
+    class Day21 : TestBaseClass<Part1Type, Part2Type>
+    {
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(-1, null, "Day21_test.txt")]
+        [TestCase(-1, null, "Day21.txt")]
+        public void Test1(Part1Type exp1, Part2Type? exp2, string resourceName)
+        {
+            var source = GetResource(resourceName);
+            var res = Compute(source);
+            DoAsserts(res, exp1, exp2);
+        }
+
+        private (Part1Type Part1, Part2Type Part2) Compute(string[] source)
+        {
+            var comp = new IntCodeComputer(source[0]);
+            comp.Execute();
+            var part1 = (int)comp.LastOutput;
+            return (part1, 0);
+        }
+    }
+}
