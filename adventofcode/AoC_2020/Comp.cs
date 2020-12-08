9333a74dca2647d2be47552e55a1d6c3908ea106
Jonas Högström <jonas.hogstrom@tobii.com>
2020-12-08 11:39:34 +0100

    Day 8

---
+++ adventofcode/AoC_2020/Comp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventofCode.AoC_2020
+{
+    public class Comp
+    {
+        public List<Instruction> Program { get; } = new List<Instruction>();
+
+        public Comp(IEnumerable<string> input)
+        {
+            foreach (var s in input)
+            {
+                var parts = s.Split(' ');
+                Program.Add(new Instruction((OpCodex)Enum.Parse(typeof(OpCodex), parts[0]), int.Parse(parts[1])));
+            }
+        }
+
+        public ExecutionState Execute(bool stopAtReExecute)
+        {
+            var accumulator = 0L;
+            var instructions = new int[Program.Count];
+            var ptr = 0;
+            var lastJump = -1;
+            var execCounter = 0;
+            while (ptr < Program.Count)
+            {
+                if (!Overrides.TryGetValue(ptr, out var instr))
+                    instr = Program[ptr];
+
+                if (stopAtReExecute && instructions[ptr] != 0)
+                {
+                    return new ExecutionState(accumulator, true, ptr, lastJump);
+                }
+
+                execCounter++;
+                instructions[ptr] = execCounter;
+
+                switch (instr.OpCode)
+                {
+                    case OpCodex.nop:
+                        ptr += 1;
+                        break;
+                    case OpCodex.jmp:
+                        ptr += instr.Arg;
+                        lastJump = ptr;
+                        break;
+                    case OpCodex.acc:
+                        ptr += 1;
+                        accumulator += instr.Arg;
+                        break;
+                }
+
+            }
+
+            return new ExecutionState(accumulator, false, ptr, lastJump);
+        }
+
+        public Dictionary<int, Instruction> Overrides = new Dictionary<int, Instruction>();
+        public class Instruction
+        {
+            public OpCodex OpCode { get; }
+            public int Arg { get; }
+
+            public Instruction(OpCodex opCode, int arg)
+            {
+                OpCode = opCode;
+                Arg = arg;
+            }
+
+            public Instruction Clone(OpCodex opCode)
+            {
+                return new Instruction(opCode, Arg);
+            }
+        }
+    }
+
+    public class ExecutionState
+    {
+        public long Accumulator { get; }
+        public bool ReExecuted { get; }
+        public int Ptr { get; }
+        public int LastJump { get; }
+
+        public ExecutionState(long accumulator, bool reExecuted, int ptr, int lastJump)
+        {
+            Accumulator = accumulator;
+            ReExecuted = reExecuted;
+            Ptr = ptr;
+            LastJump = lastJump;
+        }
+    }
+
+    public enum OpCodex
+    {
+        jmp, acc, nop
+    }
+}
---
+++ adventofcode/AoC_2020/Day8.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+using NUnit.Framework;
+
+namespace AdventofCode.AoC_2020
+{
+    using Part1Type = Int64;
+    using Part2Type = Int64;
+
+    [TestFixture]
+    class Day8 : TestBaseClass<Part1Type, Part2Type>
+    {
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(5, 8, "Day8_test.txt")]
+        [TestCase(1446, 1403, "Day8.txt")]
+        [Repeat(5)]
+        public void Test1(Part1Type exp1, Part2Type? exp2, string resourceName)
+        {
+            var source = GetResource(resourceName);
+            var res = ComputeWithTimer(source);
+            DoAsserts(res, exp1, exp2);
+        }
+
+        protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
+        {
+            var sw = Stopwatch.StartNew();
+
+            var c = new Comp(source);
+            LogAndReset("Parse", sw);
+            var part1 = c.Execute(true);
+            Log("*1 - lastJump: " + part1.LastJump);
+
+            LogAndReset("*1", sw);
+            ExecutionState part2 = null;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (c.Program[i].OpCode == OpCodex.nop)
+                    c.Overrides[i] = c.Program[i].Clone(OpCodex.jmp);
+                else if (c.Program[i].OpCode == OpCodex.jmp)
+                    c.Overrides[i] = c.Program[i].Clone(OpCodex.nop);
+                else continue;
+
+                var result = c.Execute(true);
+                c.Overrides.Clear();
+                if (!result.ReExecuted)
+                {
+                    part2 = result;
+                    Log("*2 - Modified op: " + i);
+                    break;
+                }
+            }
+
+            LogAndReset("*2", sw);
+            return (part1.Accumulator, part2.Accumulator);
+        }
+
+    }
+
+
+}
