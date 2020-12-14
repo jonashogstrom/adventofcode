30dc050d8f8f2f8da9967dd444f2a256c98ee950
Jonas Högström <jonas.hogstrom@tobii.com>
2020-12-14 08:57:06 +0100

    day 13 and 14

--- adventofcode/AoC_2020/Day13.cs
+++ adventofcode/AoC_2020/Day13.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
 using NUnit.Framework;
 
 namespace AdventofCode.AoC_2020
@@ -13,9 +15,23 @@ namespace AdventofCode.AoC_2020
         public bool Debug { get; set; }
 
         [Test]
-        [TestCase(-1, null, "Day13_test.txt")]
-        [TestCase(-1, null, "Day13.txt")]
-        public void Test1(Part1Type exp1, Part2Type? exp2, string resourceName)
+        [TestCase(295, 1068781, "Day13_test.txt")]
+        [TestCase(2845, null, "Day13.txt")]
+        // [TestCase(null, 3, "3")]
+        [Repeat(5)]
+
+        [TestCase(null, 9, "3,5")]
+        [TestCase(null, 3, "3,x,5")]
+        [TestCase(null, 12, "3,x,x,5")]
+
+        [TestCase(null, 54, "3,5,7")]
+
+        [TestCase(null, 3417, "17,x,13,19")]
+        [TestCase(null, 754018, "67,7,59,61")]
+        [TestCase(null, 779210, "67,x,7,59,61")]
+        [TestCase(null, 1261476, "67,7,x,59,61")]
+        [TestCase(null, 1202161486, "1789,37,47,1889")]
+        public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName)
         {
             var source = GetResource(resourceName);
             var res = ComputeWithTimer(source);
@@ -25,21 +41,68 @@ namespace AdventofCode.AoC_2020
         protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
         {
             var part1 = 0;
-            var part2 = 0;
+            var part2 = 0L;
 
             var sw = Stopwatch.StartNew();
 
+            var time = source.Length == 2 ? int.Parse(source[0]) : -1;
+            var buses = source.Last().Split(',').Select(s => s == "x" ? -1 : int.Parse(s)).ToList();
+
             LogAndReset("Parse", sw);
 
+
+            var first = int.MaxValue;
+            var selectedbus = -1;
+            foreach (var bus in buses.Where(b => b != -1))
+            {
+                var x = time / bus;
+                var next = (x + 1) * bus;
+                if (next < first)
+                {
+                    first = next;
+                    selectedbus = bus;
+                }
+            }
+
+            part1 = selectedbus * (first - time);
             LogAndReset("*1", sw);
 
+            var busNumbers = new System.Collections.Generic.List<int>();
+            var busOffsets = new System.Collections.Generic.List<int>();
+            for (int i = 0; i < buses.Count; i++)
+            {
+                if (buses[i] != -1)
+                {
+                    busNumbers.Add(buses[i]);
+                    busOffsets.Add(i);
+                }
+            }
+
+            var factors = new System.Collections.Generic.List<long>();
+
+            for (var i = 0; i < busNumbers.Count - 1; i++)
+            {
+                var init = 0L;
+                var xx = 1L;
+                for (var x = 0; x < factors.Count; x++)
+                {
+                    xx *= busNumbers[x];
+                    init += factors[x] * xx;
+                }
+
+                xx *= busNumbers[i];
+                var t = 1L;
+                while ((init + t * xx + busOffsets[i + 1]) % busNumbers[i + 1] != 0)
+                {
+                    t += 1;
+                }
+                part2 = init + t * xx;
+                factors.Add(t);
+            }
+
             LogAndReset("*2", sw);
+
             return (part1, part2);
         }
     }
 }
-
-// var comp = new IntCodeComputer(source[0]);
-// comp.Execute();
-// var part1 = (int)comp.LastOutput;
-// return (part1, 0);
---
+++ adventofcode/AoC_2020/Day14.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using NUnit.Framework;
+
+namespace AdventofCode.AoC_2020
+{
+    using Part1Type = Int64;
+    using Part2Type = Int64;
+
+    // not 319487199375
+
+    [TestFixture]
+    class Day14 : TestBaseClass<Part1Type, Part2Type>
+    {
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(165, null, "Day14_test.txt")]
+        [TestCase(null, 208, "Day14_test2.txt")]
+        [TestCase(17765746710228, 4401465949086, "Day14.txt")]
+        public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName)
+        {
+            var source = GetResource(resourceName);
+            var res = ComputeWithTimer(source);
+            DoAsserts(res, exp1, exp2, resourceName);
+        }
+
+        protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
+        {
+            var part1 = 0L;
+            var part2 = 0L;
+
+            var sw = Stopwatch.StartNew();
+
+            LogAndReset("Parse", sw);
+            var mem = ExecutePart1(source);
+
+            part1 = mem.Values.Sum();
+
+            LogAndReset("*1", sw);
+            mem = ExecutePart2(source);
+
+            part2 = mem.Values.Sum();
+
+            LogAndReset("*2", sw);
+            return (part1, part2);
+        }
+
+        private Dictionary<long, long> ExecutePart2(string[] source)
+        {
+            string mask = "";
+            var mem = new Dictionary<long, long>();
+            foreach (var s in source)
+            {
+                var parts = s.Split('=');
+                var instr = parts[0].Trim();
+                var arg = parts[1].Trim();
+                if (instr == "mask")
+                {
+                    mask = arg;
+                }
+                else
+                {
+                    var addr = long.Parse(instr.Split('[', ']')[1]);
+                    var value = long.Parse(arg);
+                    var temp =  ApplyMask2(addr, mask);
+                    foreach(var a in temp)
+                        mem[a] = value;
+                }
+            }
+
+            return mem;
+        }
+
+        private IEnumerable<long> ApplyMask2(long addr, string mask)
+        {
+            var res = new List<long> {addr};
+            for (int i = 0; i < mask.Length; i++)
+            {
+                var bit = (1L << 35 - i);
+
+                var m = mask[i];
+                if (m == '1')
+                {
+                    for (int p = 0; p < res.Count; p++)
+                        res[p] = res[p] | bit;
+                }
+                else if (m == 'X')
+                {
+                    var tempRes = new List<long>();
+                    var count = res.Count;
+                    for (int p = 0; p < count; p++)
+                    {
+                        var temp = res[p];
+                        res[p] = temp | bit;
+                        res.Add(res[p]-bit);
+                    }
+                }
+            }
+            return res;
+        }
+
+        private Dictionary<long, long> ExecutePart1(string[] source)
+        {
+            string mask = "";
+            var mem = new Dictionary<long, long>();
+            foreach (var s in source)
+            {
+                var parts = s.Split('=');
+                var instr = parts[0].Trim();
+                var arg = parts[1].Trim();
+                if (instr == "mask")
+                {
+                    mask = arg;
+                }
+                else
+                {
+                    var addr = long.Parse(instr.Split('[', ']')[1]);
+                    var value = long.Parse(arg);
+                    value = ApplyMask(value, mask);
+                    mem[addr] = value;
+                }
+            }
+
+            return mem;
+        }
+
+        private long ApplyMask(long value, string mask)
+        {
+            var res = value;
+            for (int i = 0; i < mask.Length; i++)
+            {
+                var bit = (1L << 35 - i);
+
+                if (mask[i] == '1')
+                {
+                    res = res | bit;
+                }
+                else if (mask[i] == '0')
+                {
+                    if (((res & bit) == bit))
+                    {
+                        res -= bit;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
