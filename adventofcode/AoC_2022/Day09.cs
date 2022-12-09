3521620f6508a6f5822a9c9ffe4884280294e5f5
Jonas Högström <jonas.hogstrom@pobox.com>
2022-12-09 08:06:37 +0100

    day9 - fixed both part 1 and 2

--- adventofcode/AoC_2022/Day09.cs
+++ adventofcode/AoC_2022/Day09.cs
@@ -16,9 +16,10 @@ namespace AdventofCode.AoC_2022
         public bool Debug { get; set; }
 
         [Test]
-        [TestCase(null, 1, "Day09_test.txt")]
+        [TestCase(13, 1, "Day09_test.txt")]
         [TestCase(null, 36, "Day09_test2.txt")]
-        [TestCase(6376, null, "Day09.txt")]
+        [TestCase(null, null, "Day09_testjay1.txt")]
+        [TestCase(6376, 2607, "Day09.txt")]
         public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName)
         {
             LogLevel = resourceName.Contains("test") ? 20 : -1;
@@ -32,22 +33,42 @@ namespace AdventofCode.AoC_2022
             Part1Type part1 = 0;
             Part2Type part2 = 0;
             var sw = Stopwatch.StartNew();
-            // var prev = new Coord(0, 0);
-            // var k.Pos = new Coord(0, 0);
 
-            var visited = new SparseBuffer<bool>();
-            var stepCounter = 0;
+            var knots1 = new List<Knot>();
+
+            LogAndReset("Parse", sw);
+            knots1.Add(new Knot(Coord.Origin, 'H'));
+            knots1.Add(new Knot(Coord.Origin, 'T'));
+
+            part1 = MakeMoves(source, knots1);
+
+            LogAndReset("*1", sw);
+
+            var knots2 = new List<Knot>();
             var ropeLength = 10;
-            var knots = new List<Knot>();
-            knots.Add(new Knot(Coord.Origin, 'H'));
+            knots2.Add(new Knot(Coord.Origin, 'H'));
             for (int x = 1; x < ropeLength; x++)
             {
-                knots.Add(new Knot(Coord.Origin, x.ToString()[0]));
+                knots2.Add(new Knot(Coord.Origin, x.ToString()[0]));
             }
+
+            part2 = MakeMoves(source, knots2);
+
+            LogAndReset("*2", sw);
+
+            return (part1, part2);
+        }
+
+        private int MakeMoves(string[] source, List<Knot> knots)
+        {
+            var visited = new SparseBuffer<bool>();
             visited[knots.Last().Pos] = true;
 
+            var stepCounter = 0;
+            var instrCounter = 0;
             foreach (var instr in source)
             {
+                instrCounter++;
                 var parts = instr.Split(' ');
                 var dir = Coord.trans2Coord[parts[0][0]];
                 var dist = int.Parse(parts[1]);
@@ -69,13 +90,13 @@ namespace AdventofCode.AoC_2022
                         {
                             // no action -- corners
                         }
-                        else if (k.Pos.Y == prev.Y )
+                        else if (k.Pos.Y == prev.Y)
                         {
                             if (k.Pos.IsWestOf(prev))
                                 k.Pos = k.Pos.Move(Coord.E);
-                            else 
+                            else
                                 k.Pos = k.Pos.Move(Coord.W);
-                        } 
+                        }
                         else if (k.Pos.X == prev.X)
                         {
                             if (k.Pos.IsNorthOf(prev))
@@ -107,30 +128,29 @@ namespace AdventofCode.AoC_2022
                                 }
                             }
                         }
+
                         prev = k.Pos;
                     }
 
                     visited[knots.Last().Pos] = true;
                     stepCounter++;
-                    if (instr == "R 17")
-                        LogMap(visited, knots, stepCounter, instr);
+                    if (instr == "R 17" && Debug)
+                        LogMap(visited, knots, stepCounter, instr, instrCounter);
                 }
 
-                LogMap(visited, knots, stepCounter, instr);
-                if (instr == "R 17")
+                if (Debug)
+                    LogMap(visited, knots, stepCounter, instr, instrCounter);
+                if (visited.Keys.Count() > 1)
                 {
                     Log("here");
                 }
             }
-            LogAndReset("Parse", sw);
-            part2 = visited.Keys.Count();
-            LogAndReset("*1", sw);
-            LogAndReset("*2", sw);
 
-            return (part1, part2);
+            return visited.Keys.Count();
         }
 
-        private void LogMap(SparseBuffer<bool> visited, List<Knot> knots, int stepCounter, string instr)
+        private void LogMap(SparseBuffer<bool> visited, List<Knot> knots, int stepCounter, string instr,
+            int instrCounter)
         {
             var map = new SparseBuffer<char>('.');
             foreach (var x in visited.Keys)
@@ -142,7 +162,7 @@ namespace AdventofCode.AoC_2022
                 map[k.Pos] = k.Name;
             }
 
-            Log("======= step: " + stepCounter + " - " + instr);
+            Log($"======= Instr: {instr} (#{instrCounter}) - step {stepCounter}" );
             Log(map.ToString(c => c.ToString()));
         }
     }
