03d7222d8ab3c0a767a50f67b8fc282e9fce6a18
Jonas Högström <jonas.hogstrom@pobox.com>
2023-12-03 00:01:47 +0100

    day 3 clean

---
+++ adventofcode/AoC_2023/Day03.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace AdventofCode.AoC_2023
+{
+    // to use string-types, change baseclass to TestBaseClass2 and remove a bunch of ? in the methods below
+    using Part1Type = Int64;
+    using Part2Type = Int64;
+
+    [TestFixture]
+    class Day03 : TestBaseClass<Part1Type, Part2Type>
+    {
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(9999999, null, "Day03_test.txt")]
+        [TestCase(9999999, null, "Day03.txt")]
+        public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName)
+        {
+            LogLevel = resourceName.Contains("test") ? 20 : -1;
+            var source = GetResource(resourceName);
+            var res = ComputeWithTimer(source);
+            DoAsserts(res, exp1, exp2, resourceName);
+        }
+
+        protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
+        {
+            Part1Type part1 = 0;
+            Part2Type part2 = 0;
+            var sw = Stopwatch.StartNew();
+
+            // parse input here
+
+            LogAndReset("Parse", sw);
+
+            // solve part 1 here
+
+            LogAndReset("*1", sw);
+
+            // solve part 2 here
+
+            LogAndReset("*2", sw);
+
+            return (part1, part2);
+        }
+    }
+}
