8c0bb0cd424e57d11a283f71309559eeacc44993
Jonas Högström <jonas.hogstrom@pobox.com>
2024-12-11 23:19:28 +0100

    day 10 and 11

---
+++ adventofcode/AoC_2024/Day10.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using System.Collections.Generic;
+using AdventofCode.Utils;
+using System.Linq;
+
+
+namespace AdventofCode.AoC_2024
+{
+    // to use string-types, change baseclass to TestBaseClass2 and remove a bunch of ? in the methods below
+    using Part1Type = Int64;
+    using Part2Type = Int64;
+
+    [TestFixture]
+    class Day10 : TestBaseClass<Part1Type, Part2Type>
+    {
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(36, 81, "Day10_test.txt")]
+        [TestCase(1, null, "Day10_test0.txt")]
+        [TestCase(2, null, "Day10_test1.txt")]
+        [TestCase(4, null, "Day10_test2.txt")]
+        [TestCase(3, null, "Day10_test3.txt")]
+        [TestCase(1, 3, "Day10_test4.txt")]
+        [TestCase(538, null, "Day10.txt")]
+        public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName)
+        {
+            LogLevel = resourceName.Contains("test") ? 20 : -1;
+            var source = GetResource(resourceName);
+            var res = ComputeWithTimer(source);
+            DoAsserts(res, exp1, exp2, resourceName);
+        }
+
+        protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
+        {
+            Part1Type part1 = 0;
+            Part2Type part2 = 0;
+            var sw = Stopwatch.StartNew();
+
+            var map = source.ToSparseBufferStr(-1, c => c == '.' ? -1 : int.Parse(c.ToString()));
+            var trailheads = map.Keys.Where(k => map[k] == 0).ToArray();
+
+            LogAndReset("Parse", sw);
+
+            foreach (var t in trailheads)
+            {
+                var res = FindPeaks(map, t);
+                part1 += res.Item1;
+                part2 += res.Item2;
+            }
+
+            // solve part 1 here
+
+            LogAndReset("*1", sw);
+
+            // solve part 2 here
+
+            LogAndReset("*2", sw);
+
+            return (part1, part2);
+        }
+
+        private (long, long) FindPeaks(SparseBufferStr<int> map, CoordStr start)
+        {
+            var i = 0;
+            var positions = new DicWithDefault<CoordStr, int> { [start] = 1 };
+            while (i < 9)
+            {
+                var newPositions = new DicWithDefault<CoordStr, int>();
+                foreach (var p in positions.Keys)
+                foreach (var n in p.GenAdjacent4())
+                    if (map.InsideBounds(n) && map[n] == i + 1)
+                        newPositions[n] += positions[p];
+                positions = newPositions;
+                i++;
+            }
+
+            return (positions.Keys.Count(), positions.Values.Sum());
+        }
+    }
+}
---
+++ adventofcode/AoC_2024/Day11.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using System.Collections.Generic;
+using AdventofCode.Utils;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace AdventofCode.AoC_2024
+{
+    // to use string-types, change baseclass to TestBaseClass2 and remove a bunch of ? in the methods below
+    using Part1Type = Int64;
+    using Part2Type = Int64;
+
+    [TestFixture]
+    class Day11 : TestBaseClass<Part1Type, Part2Type>
+    {
+        private int _steps;
+        public bool Debug { get; set; }
+
+        [Test]
+        [TestCase(22, null, "Day11_test.txt", 6)]
+        [TestCase(55312, null, "Day11_test.txt", 25)]
+        [TestCase(19778, null, "Day11_test0.txt", 25)]
+        [TestCase(203609, 240954878211138, "Day11.txt", 25)]
+        public void Test1(Part1Type? exp1, Part2Type? exp2, string resourceName, int steps)
+        {
+            _steps = steps;
+            LogLevel = resourceName.Contains("test") ? 20 : -1;
+            var source = GetResource(resourceName);
+            var res = ComputeWithTimer(source);
+            DoAsserts(res, exp1, exp2, resourceName);
+        }
+
+        protected override (Part1Type? part1, Part2Type? part2) DoComputeWithTimer(string[] source)
+        {
+            Part1Type part1 = 0;
+            Part2Type part2 = 0;
+            var sw = Stopwatch.StartNew();
+
+            var gen0 = source.First().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            
+            LogAndReset("Parse", sw);
+            var p2_gen0 = new DicWithDefault<long, long>();
+            foreach (var s in gen0)
+                p2_gen0[s]++;
+            var p2_list = p2_gen0;
+            var stones = -1L;
+            for (var i = 0; i < 75; ++i)
+            {
+                p2_list = Evolve2(p2_list);
+                stones = p2_list.Keys.Sum(k => p2_list[k]);
+                if (i == (_steps - 1))
+                {
+                    part1 = stones;
+                    LogAndReset("*1", sw);
+                }
+            }
+
+            part2 = stones;
+
+            LogAndReset("*2", sw);
+
+            return (part1, part2);
+        }
+
+        private DicWithDefault<long, long> Evolve2(DicWithDefault<long, long> p2List)
+        {
+            var res = new DicWithDefault<long, long>();
+            foreach (var s in p2List.Keys)
+            {
+                var c = p2List[s];
+                if (s == 0)
+                {
+                    res[1] += c;
+                }
+                else if (s.ToString().Length % 2 == 0)
+                {
+                    var str = s.ToString();
+
+                    res[long.Parse(str[..(str.Length / 2)])] += c;
+                    res[long.Parse(str[(str.Length / 2)..])] += c;
+                }
+                else
+                {
+                    res[s * 2024] += c;
+                }
+            }
+
+            return res;
+        }
+
+        // algo that works for *1, but not *2
+        private List<long> Evolve(List<long> list)
+        {
+            var res = new List<long>();
+            foreach (var s in list)
+            {
+                if (s == 0)
+                {
+                    res.Add(1);
+                }
+                else if (s.ToString().Length % 2 == 0)
+                {
+                    var str = s.ToString();
+                    res.Add(long.Parse(str[..(str.Length / 2)]));
+                    res.Add(long.Parse(str[(str.Length / 2)..]));
+                }
+                else
+                {
+                    res.Add(s * 2024);
+                }
+            }
+            return res;
+        }
+    }
+}
